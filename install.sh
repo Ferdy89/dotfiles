eb8869b1bca5214e19256521cd412e8d22e0f9f8
Fernando Seror <ferdy89@gmail.com>
2018-05-24 17:37:54 -0500

    Attempt to fix the install.sh script

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-ln -s dotfiles/.vim* ~/
-ln -s dotfiles/.tmux* ~/
+pushd ~
+
+ln -s dotfiles/.vimrc .
+ln -s dotfiles/.vim .
+ln -s dotfiles/.tmux.conf .
+
+mkdir -p .config/nvim
 
 cat > ~/.config/nvim/init.vim <<EOF
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
 EOF
+
+popd
