b6c90bf671c91555971886f0353b4b3d158f3b8e
Fernando Seror Garcia <ferdynton@gmail.com>
2023-10-09 14:31:10 +0200

    Fix annoying "no matches found" when there are no machine-specific files

---
+++ zsh/.oh-my-zsh/custom/this_machine/README.zsh
@@ -0,0 +1,7 @@
+# This directory is excluded from the dotfiles repo, which means any files in here
+# will not be persisted across other installations of the dotfiles.
+#
+# Place here anything that is only relevant to this machine and not to others.
+#
+# This file is purposefully a zsh file so the glob pattern in load_nested.zsh
+# always finds something instead of printing an annoying error
